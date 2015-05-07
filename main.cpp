ddbf7b0048759b0711a8f4dd335397d677a65f55
Jonah Woodbury <jonahwoodbury@gmail.com>
2015-05-07 19:24:39 -0400

    Improved comments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 
 // This program asks for an unlimited ammount of inputs, puts each of them into an index of a vector, and shows you the greatest and least numbers you entered.
 
+// Commenting this was tiring!
+
 
 #include <iostream>
 #include <vector>
@@ -23,18 +25,25 @@ int main(){
     std::cout << "Enter some numbers:\n";
     
     while(std::cin >> x){
-    // I don't really understand how this while loop works, and though other people use this it seems to behave strangely for me. I think it might be the problem since entering things other than integers (which I am generally required to do in order to get anything to happen) will probably effect the program's ability to determine if an index is greater than another index.
+    /* I don't really understand how this while loop works, and though other people use this it seems to behave strangely for
+     me. I think it might be the problem since entering things other than integers (which I am generally required to do in
+     order to get anything to happen) will probably effect the program's ability to determine if an index is greater than
+     another index. */
         
         
         // notes on what I've noticed while the program is in this loop:
         
-        // When I type multiple numbers (separated by spaces) and press the 'enter' key the command line still expects input.
+        /* When I type multiple numbers (separated by spaces) and press the 'enter' key the command line still expects input. */
         
-        // When I type a letter after the numbers, the program doesn't stop and I get that same error that shows a 'breakpoint,' this time down at the for loop. When I type a letter before the numbers, the program finishes and tells me that the vector is empty.
+        /* When I type a letter after the numbers, the program doesn't stop and I get that same error that shows a
+         'breakpoint,' this time down at the for loop. When I type a letter before the numbers, the program finishes and tells
+         me that the vector is empty. */
         
-        // When I type in a really long number (something like 22 characters, and then just hit enter) the program finishes, and says that the vector is empty. When I type two long numbers, it does the same. This is the only way that I can get the program to respond with only numbers in the stream.
+        /* When I type in a really long number (something like 22 characters, and then just hit enter) the program finishes,
+         and says that the vector is empty. When I type two long numbers, it does the same. This is the only way that I can
+         get the program to respond with only numbers in the stream. */
         
-        // When I type in only letters the program finishes and tells me that the vector is empty.
+        /* When I type in only letters the program finishes and tells me that the vector is empty. */
    
         
     // v.push_back(x) defnitely works though.
