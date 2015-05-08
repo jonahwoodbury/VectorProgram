2c845b9b8181f45782e458aed520e7f83d6c4959
Jonah Woodbury <jonahwoodbury@gmail.com>
2015-05-07 23:10:45 -0400

    program works now

--- main.cpp
+++ main.cpp
@@ -7,57 +7,41 @@
 //
 
 
-
-
 // This program asks for an unlimited ammount of inputs, puts each of them into an index of a vector, and shows you the greatest and least numbers you entered.
-
-// Commenting this was tiring!
-
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 int main(){
     
-    int x;
-    std::vector<int> v; // I was having some error on this line that involved the word 'breakpoint' until it went away for no reason.
-    
+    std::vector<int> v;
     std::cout << "Enter some numbers:\n";
     
-    while(std::cin >> x){
-    /* I don't really understand how this while loop works, and though other people use this it seems to behave strangely for
-     me. I think it might be the problem since entering things other than integers (which I am generally required to do in
-     order to get anything to happen) will probably effect the program's ability to determine if an index is greater than
-     another index. */
+    while(true){
+        std::string input = "";
+        int arrayInput = 0;
         
+// This getline stuff works
         
-        // notes on what I've noticed while the program is in this loop:
-        
-        /* When I type multiple numbers (separated by spaces) and press the 'enter' key the command line still expects input. */
-        
-        /* When I type a letter after the numbers, the program doesn't stop and I get that same error that shows a
-         'breakpoint,' this time down at the for loop. When I type a letter before the numbers, the program finishes and tells
-         me that the vector is empty. */
-        
-        /* When I type in a really long number (something like 22 characters, and then just hit enter) the program finishes,
-         and says that the vector is empty. When I type two long numbers, it does the same. This is the only way that I can
-         get the program to respond with only numbers in the input stream. */
-        
-        /* When I type in only letters the program finishes and tells me that the vector is empty. */
-   
-        
-    // v.push_back(x) defnitely works though.
-            v.push_back(x);
+        getline(std::cin, input);
+        std::stringstream myStream(input);
+        while(myStream >> arrayInput){
+            v.push_back(arrayInput);
+        }
+    break;
     }
     
     std::size_t i = 0;
     std::size_t greatest_index = 0;
     std::size_t least_index = 0;
 
-    for (; i < v.size(); i += 1) { // 'i' doesn't iterate here
-        std::cout << "vector size = " << v.size() << std::endl << "i = " << i << std::endl; // I added this line to tell whether or not 'v' and 'i' were being added to. 'v' usually is, but 'i' never is.
-        if(v[i] > v[greatest_index]) greatest_index = i; std::cout << "New greatest index " << v[greatest_index]; // I get that 'breakpoint' error right at this line.
-        if(v[i] < v[least_index]) least_index = i; std::cout << "New least index " << v[least_index];
+    for (; i < v.size(); i += 1) {
+        std::cout << "vector size = " << v.size() << std::endl << "i = " << i << std::endl;
+        
+        if(v[i] > v[greatest_index]) greatest_index = i; // std::cout << "New greatest index " << v[greatest_index] << std::endl;
+        if(v[i] < v[least_index]) least_index = i; // std::cout << "New least index " << v[least_index] << std::endl;
+    
     }
         if(v.empty()){
             std::cout << "Vector is empty.\n";
